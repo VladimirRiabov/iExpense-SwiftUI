e6791a0f3fca3521dd3ccc8e4b6fdd1c20f11d9f
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-13 00:28:53 +0300

    Initial Commit

---
+++ iExpense SwiftUI/iExpense_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_SwiftUIApp.swift
+//  iExpense SwiftUI
+//
+//  Created by Владимир Рябов on 13.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
